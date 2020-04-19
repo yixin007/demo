1fb9bdaf6ee6a8c810a1e2f8697f8a2615e95f0b
jack <jack@jackdeMacBook-Air.local>
2020-04-19 18:53:58 +0800

    dddd

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Hello {
+    @GetMapping("/hello")
+    public String hello(){
+        return "dddddddd";
+    }
+}
